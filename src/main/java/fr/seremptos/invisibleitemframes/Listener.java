d9229c69bb6064c1696f1ef0491dc618d97244d5
Seremptos <thomasf60120@gmail.com>
2023-09-08 22:01:49 +0200

    On ajoute la possibilité de récupérer les cadres invisibles (activé par défaut dans la config)

--- src/main/java/fr/seremptos/invisibleitemframes/Listener.java
+++ src/main/java/fr/seremptos/invisibleitemframes/Listener.java
@@ -1,13 +1,27 @@
 package fr.seremptos.invisibleitemframes;
 
 import de.tr7zw.nbtapi.NBTCompound;
+import de.tr7zw.nbtapi.NBTEntity;
 import de.tr7zw.nbtapi.NBTItem;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.hanging.HangingBreakEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.ItemStack;
 
 
 public class Listener implements org.bukkit.event.Listener {
+
+    private Main main;
+
+    public Listener(Main main) {
+        this.main = main;
+    }
+
+
     @EventHandler
     public void onCraft(PrepareItemCraftEvent event) {
         if(event.getInventory().getMatrix().length != 9) return; // On check si on est bien dans un établi.
@@ -20,6 +34,21 @@ public class Listener implements org.bukkit.event.Listener {
             if(coupound != null && coupound.hasTag("Invisible")) {
                 event.getInventory().setResult(null);
             }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onHit(HangingBreakEvent event) {
+        if (!event.isCancelled() && event.getEntity() instanceof ItemFrame itemFrame && main.getConfig().getBoolean("override-item-frame-drop", true)) {
+            event.setCancelled(true);
+            NBTEntity nbt = new NBTEntity(itemFrame);
+            Location loc = event.getEntity().getLocation();
+            if (nbt.hasTag("Invisible")) {
+                loc.getWorld().dropItem(loc, Main.getInvisibleFrame(
+                        new ItemStack(Material.matchMaterial(event.getEntity().getType().toString())))
+                );
+            }
+            itemFrame.remove();
         }
     }
 }
--- src/main/java/fr/seremptos/invisibleitemframes/Main.java
+++ src/main/java/fr/seremptos/invisibleitemframes/Main.java
@@ -16,11 +16,12 @@ public final class Main extends JavaPlugin {
 
     public static List<Material> frames = List.of(Material.ITEM_FRAME, Material.GLOW_ITEM_FRAME);
 
+
     @Override
     public void onEnable() {
         addInvisibleItemFrameRecipe();
-        Bukkit.getPluginManager().registerEvents(new Listener(), this);
-
+        Bukkit.getPluginManager().registerEvents(new Listener(this), this);
+        saveDefaultConfig();
     }
 
     @Override
@@ -28,19 +29,12 @@ public final class Main extends JavaPlugin {
     }
 
     public void addInvisibleItemFrameRecipe() {
-        for(Material material : frames) {
-            ItemStack frame = new ItemStack(material);
-            NBTItem iframe_nbt = new NBTItem(frame);
-            iframe_nbt.addCompound("EntityTag").setBoolean("Invisible", true);
-
-            ItemStack iframe = iframe_nbt.getItem();
-            iframe.editMeta(meta -> meta.displayName(MiniMessage.miniMessage().deserialize("<lang:"+ frame.translationKey()+ "> invisible").decoration(TextDecoration.ITALIC, false)));
-
-            NamespacedKey key = new NamespacedKey(this, "invisible_"+frame.getType());
-            ShapedRecipe recipe = new ShapedRecipe(key, iframe);
+        for(Material frame : frames) {
+            NamespacedKey key = new NamespacedKey(this, "invisible_"+frame);
+            ShapedRecipe recipe = new ShapedRecipe(key, getInvisibleFrame(new ItemStack(frame)));
             recipe.shape(" M ", "MIM", " M ");
             recipe.setIngredient('M', Material.PHANTOM_MEMBRANE);
-            recipe.setIngredient('I', frame.getType());
+            recipe.setIngredient('I', frame);
 
             if (Bukkit.addRecipe(recipe)) {
                 getLogger().info("Recette ajoutée avec succès ! ("+key.asString()+")");
@@ -48,5 +42,13 @@ public final class Main extends JavaPlugin {
                 getLogger().warning("La recette n'a pas pu être ajoutée ! ("+key.asString()+")");
             }
         }
+    }
+
+    public static ItemStack getInvisibleFrame(ItemStack frame) {
+        NBTItem iframe_nbt = new NBTItem(frame);
+        iframe_nbt.addCompound("EntityTag").setBoolean("Invisible", true);
+        ItemStack iframe = iframe_nbt.getItem();
+        iframe.editMeta(meta -> meta.displayName(MiniMessage.miniMessage().deserialize("<lang:"+ frame.translationKey()+ "> invisible").decoration(TextDecoration.ITALIC, false)));
+        return iframe;
     }
 }
